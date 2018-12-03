1a731596de6891405c605592d67116ce8205f0a5
duongta154 <dta1@villanova.edu>
2018-12-02 22:14:44 -0500

    Modified version of The Six Natures Project
    - Developed an informative website to introduce the six most interesting things in every Japan aspect
    - Follow this link for a full version:
    https://sixnatures-japan.com

---
+++ search.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Search Results</title>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <link rel="styelsheet" href="OutLayIndex.css">
+</head>
+<body>
+    <?php
+        $str = "Top 6 Japanese Interesting Food";
+        echo $str;
+    ?>
+</body>
+</html>
